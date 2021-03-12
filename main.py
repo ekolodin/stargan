891bf36bede426d39bd5a0a058d1e213a22b5cc1
ekolodin <ekolodin98@gmail.com>
2021-03-12 14:38:07 +0300

    update config

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from torch.backends import cudnn
 def str2bool(v):
     return v.lower() in ('true')
 
+
 def main(config):
     # For fast training.
     cudnn.benchmark = True
@@ -23,32 +24,16 @@ def main(config):
         os.makedirs(config.result_dir)
 
     # Data loader.
-    celeba_loader = None
-    rafd_loader = None
-
-    if config.dataset in ['CelebA', 'Both']:
-        celeba_loader = get_loader(config.celeba_image_dir, config.attr_path, config.selected_attrs,
-                                   config.celeba_crop_size, config.image_size, config.batch_size,
-                                   'CelebA', config.mode, config.num_workers)
-    if config.dataset in ['RaFD', 'Both']:
-        rafd_loader = get_loader(config.rafd_image_dir, None, None,
-                                 config.rafd_crop_size, config.image_size, config.batch_size,
-                                 'RaFD', config.mode, config.num_workers)
-    
+    celeba_loader = get_loader(config.celeba_image_dir, config.attr_path, config.selected_attrs,
+                               config.celeba_crop_size, config.image_size, config.batch_size, config.mode)
 
     # Solver for training and testing StarGAN.
-    solver = Solver(celeba_loader, rafd_loader, config)
+    solver = Solver(celeba_loader, config)
 
     if config.mode == 'train':
-        if config.dataset in ['CelebA', 'RaFD']:
-            solver.train()
-        elif config.dataset in ['Both']:
-            solver.train_multi()
+        solver.train()
     elif config.mode == 'test':
-        if config.dataset in ['CelebA', 'RaFD']:
-            solver.test()
-        elif config.dataset in ['Both']:
-            solver.test_multi()
+        solver.test()
 
 
 if __name__ == '__main__':
@@ -56,9 +41,7 @@ if __name__ == '__main__':
 
     # Model configuration.
     parser.add_argument('--c_dim', type=int, default=5, help='dimension of domain labels (1st dataset)')
-    parser.add_argument('--c2_dim', type=int, default=8, help='dimension of domain labels (2nd dataset)')
     parser.add_argument('--celeba_crop_size', type=int, default=178, help='crop size for the CelebA dataset')
-    parser.add_argument('--rafd_crop_size', type=int, default=256, help='crop size for the RaFD dataset')
     parser.add_argument('--image_size', type=int, default=128, help='image resolution')
     parser.add_argument('--g_conv_dim', type=int, default=64, help='number of conv filters in the first layer of G')
     parser.add_argument('--d_conv_dim', type=int, default=64, help='number of conv filters in the first layer of D')
@@ -66,16 +49,14 @@ if __name__ == '__main__':
     parser.add_argument('--d_repeat_num', type=int, default=6, help='number of strided conv layers in D')
     parser.add_argument('--lambda_cls', type=float, default=1, help='weight for domain classification loss')
     parser.add_argument('--lambda_rec', type=float, default=10, help='weight for reconstruction loss')
-    parser.add_argument('--lambda_gp', type=float, default=10, help='weight for gradient penalty')
-    
+
     # Training configuration.
-    parser.add_argument('--dataset', type=str, default='CelebA', choices=['CelebA', 'RaFD', 'Both'])
+    parser.add_argument('--dataset', type=str, default='CelebA', choices=['CelebA'])
     parser.add_argument('--batch_size', type=int, default=16, help='mini-batch size')
-    parser.add_argument('--num_iters', type=int, default=200000, help='number of total iterations for training D')
+    parser.add_argument('--num_iters', type=int, default=200000, help='number of total iterations for training')
     parser.add_argument('--num_iters_decay', type=int, default=100000, help='number of iterations for decaying lr')
     parser.add_argument('--g_lr', type=float, default=0.0001, help='learning rate for G')
-    parser.add_argument('--d_lr', type=float, default=0.0001, help='learning rate for D')
-    parser.add_argument('--n_critic', type=int, default=5, help='number of D updates per each G update')
+    parser.add_argument('--d_lr', type=float, default=0.0005, help='learning rate for D')
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam optimizer')
     parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam optimizer')
     parser.add_argument('--resume_iters', type=int, default=None, help='resume training from this step')
@@ -91,18 +72,18 @@ if __name__ == '__main__':
     parser.add_argument('--use_tensorboard', type=str2bool, default=True)
 
     # Directories.
-    parser.add_argument('--celeba_image_dir', type=str, default='data/celeba/images')
-    parser.add_argument('--attr_path', type=str, default='data/celeba/list_attr_celeba.txt')
-    parser.add_argument('--rafd_image_dir', type=str, default='data/RaFD/train')
-    parser.add_argument('--log_dir', type=str, default='stargan/logs')
-    parser.add_argument('--model_save_dir', type=str, default='stargan/models')
-    parser.add_argument('--sample_dir', type=str, default='stargan/samples')
-    parser.add_argument('--result_dir', type=str, default='stargan/results')
+    parser.add_argument('--celeba_image_dir', type=str, required=True)
+    parser.add_argument('--attr_path', type=str, required=True)
+    parser.add_argument('--log_dir', type=str, default='logs')
+    parser.add_argument('--model_save_dir', type=str, default='models')
+    parser.add_argument('--sample_dir', type=str, default='samples')
+    parser.add_argument('--result_dir', type=str, default='results')
 
     # Step size.
     parser.add_argument('--log_step', type=int, default=10)
+    parser.add_argument('--fid_step', type=int, default=5000)
     parser.add_argument('--sample_step', type=int, default=1000)
-    parser.add_argument('--model_save_step', type=int, default=10000)
+    parser.add_argument('--model_save_step', type=int, default=5000)
     parser.add_argument('--lr_update_step', type=int, default=1000)
 
     config = parser.parse_args()
